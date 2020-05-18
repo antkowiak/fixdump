f91f870aa8be7b9497a65b0045b9b5bb17e931b5
Ryan Antkowiak <antkowiak@gmail.com>
2020-05-18 15:07:05 -0500

    Update script

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cat $1 |sed -E 's/^(.*) = (.*)$/"\1": "\2",/g' |sort |sed -E '/^$/d' |sed -E ':a;N;$! ba;s/,$//g'
+echo "    \"${2}\": {"
+cat $1 |sed -E 's/^(.*) = (.*)$/"\1": "\2",/g' |sort |sed -E '/^$/d' |sed -E ':a;N;$! ba;s/,$//g' |sed -E 's/^(.*)$/      \1/g'
+echo "    },"
 
