142cfb389ea01a80d0aa7c3938a1c1a8511793db
Ryan Antkowiak <antkowiak@gmail.com>
2020-05-18 15:22:15 -0500

    Update process script and json

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "    \"${2}\": {"
-cat $1 |sed -E 's/^(.*) = (.*)$/"\1": "\2",/g' |sort |sed -E '/^$/d' |sed -E ':a;N;$! ba;s/,$//g' |sed -E 's/^(.*)$/      \1/g'
+cat $1 |sed -E "s/\"/'/g" |sed -E 's/^(.*) = (.*)$/"\1": "\2",/g' |sort |sed -E '/^$/d' |sed -E ':a;N;$! ba;s/,$//g' |sed -E 's/^(.*)$/      \1/g'
 echo "    },"
 
